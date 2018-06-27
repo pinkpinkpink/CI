672576134fa4964b2802f115f4e148befbdd7639
wangyifan <15638515778@163.com>
2018-06-27 23:05:38 +0800

    函数式编程

---
+++ src/main/java/org.docker.com/Person.java
@@ -0,0 +1,46 @@
+package src.main.java.org.docker.com;
+
+public class Person {
+    private String name;
+    private int id;
+    private int age;
+    public Person(){
+        cry();
+    }
+    public void Person(String name,int id,int age){
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void cry(){
+        System.out.println("wawawa");
+
+    }
+
+}
